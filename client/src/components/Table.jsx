4b17b8e6f8fac424b759421ba8875315aa04d193
prashmak <prashant.makwana@gmail.com>
2024-06-03 20:45:20 +0530

    Deploy Netlify1

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -97,7 +97,7 @@ const Table = () => {
                 {Object.keys(item).map((col, i) => (
                   <td key={i} className='w-[80px] text-center'>
                     {col === 'orderNo' ? (
-                      <a href="/productsingle">{item[col]}</a>
+                      <NavLink href="/productsingle">{item[col]}</NavLink>
                     ) : col === 'CAD' ? (
                       <span className='flex justify-center'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
